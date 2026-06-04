ddb6fa597646e9c8b2a59485e0f87e3bd74eed04
JABERFREE <emadjaber23@gmail.com>
2026-06-04 17:26:55 +0300

    Fix buttons visibility for Motorola and all devices

--- android/app/src/main/java/com/nexapp/neonretrobowling/MainActivity.java
+++ android/app/src/main/java/com/nexapp/neonretrobowling/MainActivity.java
@@ -2,4 +2,19 @@ package com.nexapp.neonretrobowling;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+                | android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+}
